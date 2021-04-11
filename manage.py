e0d062b3ede41e3baa97ccd4d2efc0a265525c99
Daniel Aguilar <20218476+daniel-aguilar@users.noreply.github.com>
2021-04-11 01:15:40 -0600

    Upgrade to Django 3.2

--- manage.py
+++ manage.py
@@ -7,6 +7,7 @@ from dotenv import find_dotenv, load_dotenv
 
 
 def main():
+    """Run administrative tasks."""
     load_dotenv(find_dotenv())
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'contact.settings.production')
     try:
